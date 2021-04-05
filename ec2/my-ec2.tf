6a1b1405a9c0949105a45e832520369f35588d27
ptalawar <praveen.talawar@gartner.com>
2021-04-05 13:08:41 +0530

    adding ec2 module

---
+++ ec2/my-ec2.tf
@@ -0,0 +1,16 @@
+module "my-ec2-instance" {
+
+  name                   = "my-cluster"
+  instance_count         = 1
+
+  ami                    = "ami-0474be6b24f418f29"
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0bb571e36be145f3b"]
+  subnet_id              = "subnet-0c506575c370fed5f"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
