788c37aef47fd0d4d2d11bd0b4af369596144525
ptalawar <praveen.talawar@gartner.com>
2021-04-05 14:13:32 +0530

    correcting

--- ec2/my-ec2.tf
+++ ec2/my-ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my-ec2-instance" {
 
-  ami                    = "ami-0474be6b24f418f29"
+  ami                    = "ami-0bcf5425cdc1d8a85"
   instance_type          = "t2.micro"
   monitoring             = true
   vpc_security_group_ids = ["sg-0bb571e36be145f3b"]
